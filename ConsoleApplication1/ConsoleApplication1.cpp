f6e964e951d6ac2ad5b8141b06298a42d0afece4
said00000 <isip_s.m.murtazaliev@mpt.ru>
2024-01-15 19:04:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+   
+    std::cout << "У лукоморья дуб зеленый," << std::endl;
+    std::cout << "Златая цепь на дубе том," << std::endl;
+    std::cout << "и днем и ночью кот ученый" << std::endl;
+    std::cout << "Все ходит по цепи кругом." << std::endl;
+
+    return 0;
+}
